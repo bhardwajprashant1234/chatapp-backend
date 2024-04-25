a043e4f2d5730095c3bdf95c73a95b8b9ce67f5a
Prashant Bhardwaj <bhrdwjprashant@gmail.com>
2024-04-25 22:42:02 +0530

    updating cofig

--- constants/config.js
+++ constants/config.js
@@ -2,10 +2,12 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
+    "http://localhost:3000",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
+  optionSuccessStatus:200
 };
 
 const CHATTU_TOKEN = "chattu-token";
