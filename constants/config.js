f242778d68b12cac06411d30963bfeb10ec9e736
Prashant Bhardwaj <bhrdwjprashant@gmail.com>
2024-05-04 13:53:08 +0530

    config commit

--- constants/config.js
+++ constants/config.js
@@ -1,9 +1,23 @@
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:5173",
+//     "http://localhost:4173",
+//     "http://localhost:3000",
+//     "https://chatapp-frontend-swart.vercel.app"
+//   ],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+//   optionSuccessStatus:200
+// };
+
+// const CHATTU_TOKEN = "chattu-token";
+
+// export { corsOptions, CHATTU_TOKEN };
 const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    "http://localhost:3000",
-    "https://chatapp-frontend-swart.vercel.app"
+    process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
